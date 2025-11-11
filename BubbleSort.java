8c5873ffd5ba6841ad6fa0aefafcd77acc483b3b
Nianca Chetty <niancachetty102@gmail.com>
2025-11-11 16:01:48 +0200

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,6 +8,29 @@ package com.mycompany.sortingalgorithms;
  *
  * @author nianc
  */
+//----- Notes ------
+
 public class BubbleSort {
-    
+
+    public <T extends Comparable <T>> void sort(T[] arr) {
+        int n = arr.length();
+        boolean swapped;
+
+        for ( int i = 0; i < n - 1 ; i++) {
+        swapped = false;
+            for (int j = 0; j < n -1 -i; i++ ) {
+                if ( arr[j].compareTo(arr[j + 1]) > 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+        if ( !swapped ) {
+            break;
+        }
+        }
+    }
 }
+
