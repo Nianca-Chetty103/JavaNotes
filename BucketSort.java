5da6f02def05d6b30ad07052d2ed3aa96dab4e82
Nianca Chetty <niancachetty102@gmail.com>
2025-11-14 11:45:04 +0200

    Update BucketSort.java

--- BucketSort.java
+++ BucketSort.java
@@ -3,11 +3,51 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.sortingalgorithms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 
 /**
  *
  * @author nianc
  */
 public class BucketSort {
-    
+
+   public void sort(Integer[] arr) {
+        if (arr.length == 0) {   // corrected: arr.length
+            return;
+        }
+
+        // Find the maximum value
+        int maxValue = arr[0];
+        for (int value : arr) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+
+        int numBuckets = 10;
+        ArrayList<LinkedList<Integer>> buckets = new ArrayList<>(numBuckets);
+
+        // Initialize buckets
+        for (int i = 0; i < numBuckets; i++) {
+            buckets.add(new LinkedList<>());
+        }
+
+        // Distribute values into buckets
+        for (int value : arr) {
+            int bucketIndex = (int)((double)value / (maxValue) * (numBuckets - 1)); 
+            buckets.get(bucketIndex).add(value);
+        }
+
+        // Sort each bucket and merge back to array
+        int index = 0;
+        for (LinkedList<Integer> bucket : buckets) {
+            Collections.sort(bucket);
+            for (int value : bucket) {
+                arr[index++] = value;
+            }
+        }
+    }
+
 }
