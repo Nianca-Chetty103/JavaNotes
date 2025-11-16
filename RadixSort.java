1c18e5828e7542bb155513e784bd55cba061b846
Nianca Chetty <niancachetty102@gmail.com>
2025-11-16 19:40:58 +0200

    Update RadixSort.java

--- RadixSort.java
+++ RadixSort.java
@@ -3,11 +3,69 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.sortingalgorithms;
-
+import java.util.Arrays;
 /**
  *
  * @author nianc
  */
 public class RadixSort {
-    
+
+    public static void radixSort(int[] arr) {
+        int max = getMax(arr);
+
+        // Perform counting sort for every digit
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
+
+    // Get maximum value in array
+    private static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max)
+                max = num;
+        }
+        return max;
+    }
+
+    // Stable counting sort for a specific digit 
+    private static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10]; // base 10 digits
+
+        // Count occurrences of each digit
+        for (int num : arr) {
+            int digit = (num / exp) % 10;
+            count[digit]++;
+        }
+
+        // Convert count to prefix sum 
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Build output array
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = (arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
+        }
+
+        // Copy back to original array
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    // Test
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        System.out.println("Before Sorting: " + Arrays.toString(arr));
+
+        radixSort(arr);
+
+        System.out.println("After Sorting:  " + Arrays.toString(arr));
+    }
 }
+
+
