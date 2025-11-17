24be1123bc51fe91d1cb19345c0a610c9f3af233
Nianca Chetty <niancachetty102@gmail.com>
2025-11-17 15:08:12 +0200

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -9,5 +9,47 @@ package com.mycompany.sortingalgorithms;
  * @author nianc
  */
 public class QuickSort {
-    
+    // Main function to sort an array using QuickSort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);  
+            quickSort(arr, low, pivotIndex - 1);   // Sort left side
+            quickSort(arr, pivotIndex + 1, high);  // Sort right side
+        }
+    }
+
+    // Partition function (Lomuto partition scheme)
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];  // pivot element
+        int i = low - 1;        // index of smaller element
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap arr[i], arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Put pivot in the correct place
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1; // return pivot index
+    }
+
+    // Test it
+    public static void main(String[] args) {
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
+
